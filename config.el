9e65227acd95c3154ccdb1d40bf4bd622196ef7b
Holger Schurig <holger@holger.schurig.local>
2020-04-17 20:10:43 +0200

    sudo-edit: alias it to doom/sudo-this-file

--- config.el
+++ config.el
@@ -48,17 +48,8 @@
   (goto-char(point-min))
   (while (search-forward "\r" nil t) (replace-match "")))
 
-(defun sudo-edit (&optional arg)
-  "Edit currently visited file as root.
-
-With a prefix ARG prompt for a file to visit.
-Will also prompt for a file to visit if current
-buffer is not visiting a file."
-  (interactive "P")
-  (if (or arg (not buffer-file-name))
-      (find-file (concat "/sudo:root@localhost:"
-                         (read-file-name "Find file(as root): ")))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+(defalias 'sudo-edit 'doom/sudo-this-file
+   "Edit currently visited file as root.")
 
 
 
