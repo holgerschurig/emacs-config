0c4f37e93bd77ea01380c089c981194477bb3a83
Holger Schurig <holger@holger.schurig.local>
2020-08-14 16:28:17 +0200

    company: try childframe

--- init.el
+++ init.el
@@ -19,7 +19,8 @@
        ;;japanese
 
        :completion
-       company             ; the ultimate code completion backend
+       (company            ; the ultimate code completion backend
+        +childframe)       ; a nicer company UI. Emacs +26 only!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ivy                 ; a search engine for love and life
