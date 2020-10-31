cac29be249c197176c8e54d25373be0576a83800
Holger Schurig <holgerschurig@gmail.com>
2020-10-31 16:16:46 +0100

    eglot: activate

--- init.el
+++ init.el
@@ -94,7 +94,9 @@
        ;;(eval +overlay)   ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp
+       (lsp
+        -lsp-mode
+        +eglot)
        ;;macos             ; MacOS-specific commands
        magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
