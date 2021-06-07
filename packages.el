ab879f8bebb79d3b523860afbf3577332b529598
Holger Schurig <schurig@desktop.local>
2021-06-07 08:22:14 +0200

    org-pdftools: didn't install automatically anymore

--- packages.el
+++ packages.el
@@ -77,6 +77,8 @@
 (package! nswbuff               ;; https://github.com/joostkremers/nswbuff  -- buffer switching
   :recipe (:host github :repo "joostkremers/nswbuff"))
 
+(package! org-pdftools)         ;; https://github.com/fuxialexander/org-pdftools
+
 (package! ox-hugo)              ;; https://github.com/kaushalmodi/ox-hugo
 
 (package! rainbow-mode)         ;; https://github.com/emacs-straight/rainbow-mode  -- Colorize color names
