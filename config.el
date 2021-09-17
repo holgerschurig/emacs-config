2b28c827b34409cd5837c48c231dc3adc36141c5
Holger Schurig <you@example.com>
2021-09-17 22:46:57 +0200

    embark: make some actions immediate

--- config.el
+++ config.el
@@ -857,6 +857,11 @@ cursor must be sitting over a CSS-like color string, e.g. \"#ff008c\"."
   ;;  "C-;" #'embark-act)
   (define-key! :keymaps '(embark-collect-mode-map)
     "M-t" #'toggle-truncate-lines)
+
+  ;; Make some actions work without a special query
+  (cl-delete 'delete-file      embark-allow-edit-actions)
+  (cl-delete 'delete-directory embark-allow-edit-actions)
+  (cl-delete 'kill-buffer      embark-allow-edit-actions)
 )
 
 
