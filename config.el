6f775efb87927c78883bd3475a6bb82f54599c08
Holger Schurig <schurig@desktop.local>
2022-03-25 08:21:47 +0100

    my-next/prev-error: simplified

--- config.el
+++ config.el
@@ -491,28 +491,39 @@ If there are two windows displayed, act like \"C-x o\"."
   (setq set-mark-command-repeat-pop t))
 
 (defun my-next-error (&optional arg)
-  "Tries first flymake-goto-next-error, spell-fu-goto-next-error, then next-error."
   (interactive)
-  (if (and (boundp 'flymake-mode) flymake-mode)
-      (condition-case nil
-          (call-interactively #'flymake-goto-next-error arg)
-        (error (if (and (boundp 'spell-fu-mode) spell-fu-mode)
-                   (condition-case nil
-                       (call-interactively #'spell-fu-goto-next-error)
-                     (error (next-error arg)))
-                 (next-error arg))))))
+  (let ((p))
+    (ignore-errors
+      (save-excursion (call-interactively #'flymake-goto-next-error arg)
+                      ;; (message "FLYMAKE")
+                      (cl-pushnew (point) p)))
+    (ignore-errors
+      (save-excursion (call-interactively #'spell-fu-goto-next-error)
+                      ;; (message "SPELL-FU")
+                      (cl-pushnew (point) p)))
+    (ignore-errors
+      (save-excursion (call-interactively #'next-error arg)
+                      (cl-pushnew (point) p)))
+    (unless (null p)
+      (goto-char (apply 'min p)))))
 
 (defun my-previous-error (&optional arg)
-  "Tries first flymake-goto-previous-error, spell-fu-goto-previous-error, then previous-error."
   (interactive)
-  (if (and (boundp 'flymake-mode) flymake-mode)
-      (condition-case nil
-          (call-interactively #'flymake-goto-prev-error arg)
-        (error (if (and (boundp 'spell-fu-mode) spell-fu-mode)
-                   (condition-case nil
-                       (call-interactively #'spell-fu-goto-previous-error)
-                     (error (previous-error arg)))
-                 (previous-error arg))))))
+  (let ((p))
+    (ignore-errors
+      (save-excursion (call-interactively #'flymake-goto-prev-error arg)
+                      ;; (message "FLYMAKE")
+                      (cl-pushnew (point) p)))
+    (ignore-errors
+      (save-excursion (call-interactively #'spell-fu-goto-previous-error)
+                      ;; (message "SPELL-FU")
+                      (cl-pushnew (point) p)))
+    (ignore-errors
+      (save-excursion (call-interactively #'previous-error arg)
+                      (cl-pushnew (point) p)))
+    (unless (null p)
+      (goto-char (apply 'min p)))))
+
 
 (map! "C-x I" #'insert-buffer
 
