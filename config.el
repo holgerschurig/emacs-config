6bb01b4acf6e339f2d5e6e1e97936a03254675b3
Holger Schurig <you@example.com>
2022-04-03 11:10:54 +0200

    vertico: C-c k was used by consult-kmacro and vertico-repeat

--- config.el
+++ config.el
@@ -1392,6 +1392,7 @@ buffer."
 )
 
 
+
 ;;; Package: completion/vertico
 
 (after! vertico
@@ -1403,7 +1404,7 @@ buffer."
   (setq read-extended-command-predicate #'command-completion-default-include-p)
 
   (map!
-   ("C-c k" #'vertico-repeat-last)
+   ("C-c r" #'vertico-repeat)
    :map vertico-map
    ([next] nil)     ;; was next-history-element
    ([prior] nil)   ;; was previous-history-element
