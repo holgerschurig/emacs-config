e97aea8c9c0eff837b5ba77b6f3725e568d372cd
Holger Schurig <schurig@desktop.local>
2023-02-06 09:03:00 +0100

    pin/unpin various modules to make them work with emacs-29

--- packages.el
+++ packages.el
@@ -98,11 +98,14 @@
 
 (package! 0x0)                  ;; https://gitlab.com/willvaughn/emacs-0x0 -- pastebin like service
 
-;;(unpin! embark embark-consult consult vertico marginalia orderless)
+;; Keeping this pinned make consult-buffer throw an error
+(unpin! embark consult vertico marginalia orderless)
 
-;;(unpin! magit notmuch)
-
+;; Currently lots of things happening here
 (unpin! mastodon)
+
+;; Behaves weird with emacs-29
+(unpin! magit magit-popup)
 
 (disable-packages!
   better-jumper ;; looks like it only works in evil mode
